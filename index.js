e8205e970b82943e332340e7d680d2c51e91f237
Alex Bahm <alexgbahm@gmail.com>
2015-01-11 00:00:10 -0800

    Had an idea for a slightly different implementation of attain. Instead of having the modules file exist in the project root, it would exist inside attain. Whenever someone tried to attain an unregistered file, it would use sync search methods to find it, return it, and save it for next time. I also changed my mind on what packages to use when I realized glob is easier and expect.js isn't very good. Until next time, good night and good luck

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 var path = require("path"),
 	fs = require("fs"),
-	mod_file_name = "modules.json";
+	json = require("jsonfile"),
+	modules = json.readFileSync("modules.json"),
+	glob = require('glob');
 
+console.log("modules: " + JSON.stringify(modules));
 
-
-function find(mod_name) {
-	var module_path = path.join('..', '..');
-	var files = [];
-	var mod_index = 0;
-	do {
-		files = fs.readdirSync(module_path);
-		mod_index = files.indexOf(mod_file_name)
-		if(mod_index < 0) {
-			path.join(module_path, '..');
-		}
-	} while(mod_index < 0);
-
-	var modules = require(path.join(module_path, mod_file_name));
-
+function attain(mod_name) {
+	var projectRoot = path.join(__dirname, '..', '..');
+	// ^^ Should be folder containing the projects node_modules. Verify this.
 	var path = modules[mod_name];
+
 	if(typeof(path) == "undefined") {
-		throw new Error("File not recognized");
+		//file has not been found, must find it first
+
+		var pattern = projectRoot + '/**/' + mod_name + '/*';
+		var searchResults = glob.sync(pattern);
+
+		//If there are no || many results, it would be crazy to pick one randomly
+		if(searchResults.length === 0) {
+			throw new Error(mod_name + ' does not exist!');
+		} else if(searchResults.length > 1) {
+			throw new Error('There are multiple files named ' + mod_name);
+		}
+
+		//Only save the relative path.
+		path = searchResults[0].substr(projectRoot.length + 1);
+		modules[mod_name] = path;
+
+		//save changes
+		json.writeFileSync("modules.json", modules);
 	}
-	return path;
+
+	return projectRoot + path;
+	
 }
 
-
-module.exports = find;
-//module.exports.add = add;
-//module.exports.update = update;
+module.exports = attain;
